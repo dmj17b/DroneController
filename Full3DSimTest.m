9253fb379dcd89fc5a40441204ef77d11d1e6e0e
David Jay <dmj17b@my.fsu.edu>
2023-05-01 12:08:52 -0400

    Tried playing around with getting gains and testing controllability. No major progress...

--- Full3DSimTest.m
+++ Full3DSimTest.m
@@ -5,7 +5,7 @@ close all
 %% 
 
 % Initializing the Quadcopter:
-q0 = [1.5e5;1.5e5;1.5e5;1.5e5;0;0;0;0;0;0;0;0;0;0;0;0]; % q = [w1;w2;w3;w4;r;p;ya;x;y;z;dr;dp;dya;dx;dy;dz];
+q0 = [196.2569;196.2569;196.2569;196.2569;0;0;0;0;0;0;0;0;0;0;0;0]; % q = [w1;w2;w3;w4;r;p;ya;x;y;z;dr;dp;dya;dx;dy;dz];
 quad = Quadcopter(q0);
 Tf = 1.5;
 
@@ -13,6 +13,8 @@ Tf = 1.5;
 u = [20 15 5 10];    % Example input for pitch
 u = 50*[1 1.5 1 0.5];    % Example input for roll
 % u = [1.5 1 1.5 1];    % Example input for yaw
+
+u = 0.044*[1 1 1 1];
 
 
 % Call function to simulate quad dynamics based on 4 motor torques
@@ -50,9 +52,25 @@ plotStates(quad);
 %% Test Linearization
 [A, B, statesFP, inputsFP] = linearizeDynamics(quad);
 
+
 testLin = Quadcopter([statesFP(1:7); zeros(3,1); statesFP(8:end); zeros(3,1)]);
 [tout,qout,testLin] = simDynamics(testLin,inputsFP,[0 Tf]);
 
 figure();
 plotStates(testLin);
+
+Ctr = ctrb(A,B);
+poles = -1:-1:-10;
+
+K = place(A,B,poles);
+
+rank(Ctr);
+
+[tout,qout] = ode45(@(t,q) quadRotODE(t,q,K*q,quad),[0 5],quad.q0)
+
+
+
+
+
+
 
