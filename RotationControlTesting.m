b05092d7ff5e84c3ade8960aae4beacf6a398f5f
Mikeyy365 <102534240+Mikeyy365@users.noreply.github.com>
2023-05-04 11:53:44 -0400

    Rotational control works in 2/3 dimensions

--- RotationControlTesting.m
+++ RotationControlTesting.m
@@ -18,61 +18,113 @@ omegaReq = sqrt(Freq/quad.kf);
 
 qStar = [0;0;0;0;0;0];
 uStar = omegaReq*[1 1 1 1]';
-qDes = [0.01; 0;0;0;0;0];
-qi = [1;0;0;0;0;0];
+qDes = [0; 0; 0; 0;0;0];
+qi = [0;0;0.12;0;0;0];
 
 % Linearize:
-[A,B] = linearizeRot(quad,qStar,uStar)
+[A,B] = linearizeRot(quad,qStar,uStar);
 Ctr = ctrb(A,B);
 rank(Ctr)
 
 
 % Pole placement:
-poles = linspace(-1,-6,6)
-K = place(A,B,poles)
+% poles = linspace(-1,-6,6);
+% poles = [-1 -2 -3 -1 -2 -3];
+poles = [-2 -2 -2 -1 -1 -1];
+K = place(A,B,poles);
 
 % Simulate:
-[tout,qout] = ode45(@(t,q) quadRotODE(t,q,-K*(q-qStar) + uStar,quad),[0 Tf],qi);
+% options = odeset('RelTol',1000,'AbsTol',10);
+[tout,qout] = ode45(@(t,q) quadRotODE(t,q,K*(qDes + qStar - q) + uStar,quad),[0 Tf],qi);
 
-sys = ss(A-B*K,0*B,eye(6),[0]);
-
-[qLin,tLin] = initial(sys,qi,Tf)
+% h = 0.0001;
+% tEuler = 0:h:Tf;
+% q0 = qi;
+% dq = quadRotODE(0, q0, K*(qDes - qStar - q0) + uStar, quad);
+% qEuler = zeros(numel(tEuler), 6);
+% accels = zeros(numel(tEuler), 3);
+% 
+% for iter = 1:numel(tEuler)
+%     qEuler(iter, :) = q0;
+%     accels(iter, :) = dq(4:end);
+%     q0 = quadRotODE(0, q0, K*(qDes - qStar - q0) + uStar, quad).*h + q0;
+%     dq = quadRotODE(0, q0, K*(qDes - qStar - q0) + uStar, quad);
+% end
+% 
+% sys = ss(A-B*K,0*B,eye(6),[0]);
+% 
+% [qLin,tLin] = initial(sys,qi,Tf);
 
 
 figure;
 subplot(3,1,1)
 sgtitle("Nonlinear Simulation")
 plot(tout,qout(:,1))
+% ylim([-1 1]);
 xlabel("Time (sec)")
 ylabel("Roll (rad)")
 
 subplot(3,1,2)
 plot(tout,qout(:,2))
+% ylim([-1 1]);
 xlabel("Time (sec)")
 ylabel("Pitch (rad)")
 
 subplot(3,1,3)
 plot(tout,qout(:,3))
+% ylim([-1 1]);
 xlabel("Time (sec)")
 ylabel("Yaw (rad)")
 
-
-
-
-figure;
-sgtitle("Linear Simulation")
-subplot(3,1,1)
-plot(tLin,qLin(:,1))
-xlabel("Time (sec)")
-ylabel("Roll (rad)")
-
-subplot(3,1,2)
-plot(tLin,qLin(:,2))
-xlabel("Time (sec)")
-ylabel("Pitch (rad)")
-
-subplot(3,1,3)
-plot(tLin,qLin(:,3))
-xlabel("Time (sec)")
-ylabel("Yaw (rad)")
+% figure();
+% subplot(3,1,1);
+% sgtitle("Euler Simulation")
+% plot(tEuler,qEuler(:,1))
+% xlabel("Time (sec)")
+% ylabel("Roll (rad)")
+% 
+% subplot(3,1,2)
+% plot(tEuler,qEuler(:,2))
+% xlabel("Time (sec)")
+% ylabel("Pitch (rad)")
+% 
+% subplot(3,1,3)
+% plot(tEuler,qEuler(:,3))
+% xlabel("Time (sec)")
+% ylabel("Yaw (rad)")
+% 
+% figure()
+% sgtitle("Euler Accelerations");
+% subplot(3,1,1);
+% plot(tEuler, accels(:,1));
+% xlabel("Time (sec)")
+% ylabel("Roll Acceleration (rad)")
+% 
+% subplot(3,1,2);
+% plot(tEuler, accels(:,2));
+% xlabel("Time (sec)")
+% ylabel("Pitch Acceleration (rad)")
+% 
+% subplot(3,1,3);
+% plot(tEuler, accels(:,3));
+% xlabel("Time (sec)")
+% ylabel("Yaw Acceleration (rad)")
+% 
+% 
+% figure;
+% sgtitle("Linear Simulation")
+% subplot(3,1,1)
+% plot(tLin,qLin(:,1))
+% xlabel("Time (sec)")
+% ylabel("Roll (rad)")
+% 
+% subplot(3,1,2)
+% plot(tLin,qLin(:,2))
+% xlabel("Time (sec)")
+% ylabel("Pitch (rad)")
+% 
+% subplot(3,1,3)
+% plot(tLin,qLin(:,3))
+% xlabel("Time (sec)")
+% ylabel("Yaw (rad)")
 
